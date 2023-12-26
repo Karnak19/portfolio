5b24cc2a944b3c78b3ce2c1789ce83f74fbef869
basile <basile.vernouillet@origins-digital.com>
2023-12-26 14:06:43 +0100

    init

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import clsx from 'clsx'
+
+const variantStyles = {
+  primary:
+    'bg-stone-800 font-semibold text-stone-100 hover:bg-stone-700 active:bg-stone-800 active:text-stone-100/70 dark:bg-stone-700 dark:hover:bg-stone-600 dark:active:bg-stone-700 dark:active:text-stone-100/70',
+  secondary:
+    'bg-stone-50 font-medium text-stone-900 hover:bg-stone-100 active:bg-stone-100 active:text-stone-900/60 dark:bg-stone-800/50 dark:text-stone-300 dark:hover:bg-stone-800 dark:hover:text-stone-50 dark:active:bg-stone-800/50 dark:active:text-stone-50/70',
+}
+
+type ButtonProps = {
+  variant?: keyof typeof variantStyles
+} & (
+  | (React.ComponentPropsWithoutRef<'button'> & { href?: undefined })
+  | React.ComponentPropsWithoutRef<typeof Link>
+)
+
+export function Button({
+  variant = 'primary',
+  className,
+  ...props
+}: ButtonProps) {
+  className = clsx(
+    'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
+    variantStyles[variant],
+    className,
+  )
+
+  return typeof props.href === 'undefined' ? (
+    <button className={className} {...props} />
+  ) : (
+    <Link className={className} {...props} />
+  )
+}
